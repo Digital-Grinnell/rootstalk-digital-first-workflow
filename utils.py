4961c9571000ea415b88aec59fd2ccf325936baa
Digital-Grinnell <digital@grinnell.edu>
2024-04-24 09:22:07 -0500

    Improved utils.py

--- utils.py
+++ utils.py
@@ -5,6 +5,21 @@ import inspect
 import textwrap
 
 
+# state(key) - Return the value of st.session_state[key] or False
+# -------------------------------------------------------------------------------
+def state(key):
+    try:
+        if st.session_state[key]:
+            return st.session_state[key]
+        else:
+            return False
+    except Exception as e:
+        # st.exception(f"Exception: {e}")
+        return False
+
+
+# show_session( ) - Display st.session_state 
+# -------------------------------------------------------------------------------
 def show_session( ):
     """Showing the st.session_state."""
     show_session = st.sidebar.checkbox("Show session_state", True)
@@ -14,6 +29,8 @@ def show_session( ):
         st.write("st.session_state object:", st.session_state)
 
 
+# show_code( ) - Display the named code 
+# -------------------------------------------------------------------------------
 def show_code(main):
     """Showing the page code."""
     show_code = st.sidebar.checkbox("Show code", True)
